0292b8dba1d4c89c4e06c1cf5d2eaad05598d7e3
simple-sifu <kattyntommy@gmail.com>
2023-04-28 18:19:36 -0400

    added product carousel

--- client/Products/ProductCarousel.jsx
+++ client/Products/ProductCarousel.jsx
@@ -4,170 +4,44 @@
 2. if you want to change the dimensions of the slides you will need to edit the slideWidth variable here 👇 and the $slide-width variable in the CSS.
 ************************************/
 import "../styles/ProductCarousel.scss";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
-const slideWidth = 30;
+function ProductCarousel() {
+  const [startIndex, setStartIndex] = useState(0);
+  const [data, error] = useSelector((state) => [state.products.data]);
+  console.log("ProductCarousel -> data", data, ", data.length =", data.length);
+  const endIndex = startIndex + 3;
 
-const _items = [
-  {
-    player: {
-      title: "Efren Reyes",
-      desc: 'Known as "The Magician", Efren Reyes is well regarded by many professionals as the greatest all around player of all time.',
-      image: "https://i.postimg.cc/RhYnBf5m/er-slider.jpg",
-    },
-  },
-  {
-    player: {
-      title: "Ronnie O'Sullivan",
-      desc: "Ronald Antonio O'Sullivan is a six-time world champion and is the most successful player in the history of snooker.",
-      image: "https://i.postimg.cc/qBGQNc37/ro-slider.jpg",
-    },
-  },
-  {
-    player: {
-      title: "Shane Van Boening",
-      desc: 'The "South Dakota Kid" is hearing-impaired and uses a hearing aid, but it has not limited his ability.',
-      image: "https://i.postimg.cc/cHdMJQKG/svb-slider.jpg",
-    },
-  },
-  {
-    player: {
-      title: "Mike Sigel",
-      desc: 'Mike Sigel or "Captain Hook" as many like to call him is an American professional pool player with over 108 tournament wins.',
-      image: "https://i.postimg.cc/C12h7nZn/ms-1.jpg",
-    },
-  },
-  {
-    player: {
-      title: "Willie Mosconi",
-      desc: 'Nicknamed "Mr. Pocket Billiards," Willie Mosconi was among the first Billiard Congress of America Hall of Fame inductees.',
-      image: "https://i.postimg.cc/NfzMDVHP/willie-mosconi-slider.jpg",
-    },
-  },
-];
-
-const length = _items.length;
-_items.push(..._items);
-
-const sleep = (ms = 0) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const createItem = (position, idx) => {
-  const item = {
-    styles: {
-      transform: `translateX(${position * slideWidth}rem)`,
-    },
-    player: _items[idx].player,
+  const handlePrevClick = () => {
+    setStartIndex(Math.max(startIndex - 1, 0));
   };
 
-  switch (position) {
-    case length - 1:
-    case length + 1:
-      item.styles = { ...item.styles, filter: "grayscale(1)" };
-      break;
-    case length:
-      break;
-    default:
-      item.styles = { ...item.styles, opacity: 0 };
-      break;
-  }
+  const handleNextClick = () => {
+    setStartIndex(Math.min(startIndex + 1, data.length - 3));
+  };
 
-  return item;
-};
-
-const CarouselSlideItem = ({ pos, idx, activeIdx }) => {
-  const item = createItem(pos, idx, activeIdx);
-
+  console.log("data.length =", data.length);
   return (
-    <li className="carousel__slide-item" style={item.styles}>
-      <div className="carousel__slide-item-img-link">
-        <img src={item.player.image} alt={item.player.title} />
-      </div>
-      <div className="carousel-slide-item__body">
-        <h4>{item.player.title}</h4>
-        <p>{item.player.desc}</p>
-      </div>
-    </li>
-  );
-};
-
-const keys = Array.from(Array(_items.length).keys());
-
-export default function ProductCarousel() {
-  const [items, setItems] = React.useState(keys);
-  const [isTicking, setIsTicking] = React.useState(false);
-  const [activeIdx, setActiveIdx] = React.useState(0);
-  const bigLength = items.length;
-
-  const prevClick = (jump = 1) => {
-    if (!isTicking) {
-      setIsTicking(true);
-      setItems((prev) => {
-        return prev.map((_, i) => prev[(i + jump) % bigLength]);
-      });
-    }
-  };
-
-  const nextClick = (jump = 1) => {
-    if (!isTicking) {
-      setIsTicking(true);
-      setItems((prev) => {
-        return prev.map((_, i) => prev[(i - jump + bigLength) % bigLength]);
-      });
-    }
-  };
-
-  const handleDotClick = (idx) => {
-    if (idx < activeIdx) prevClick(activeIdx - idx);
-    if (idx > activeIdx) nextClick(idx - activeIdx);
-  };
-
-  React.useEffect(() => {
-    if (isTicking) sleep(300).then(() => setIsTicking(false));
-  }, [isTicking]);
-
-  React.useEffect(() => {
-    setActiveIdx((length - (items[0] % length)) % length) // prettier-ignore
-  }, [items]);
-
-  return (
-    <div className="carousel__wrap">
-      <div className="carousel__inner">
-        <button
-          className="carousel__btn carousel__btn--prev"
-          onClick={() => prevClick()}
-        >
-          <i className="carousel__btn-arrow carousel__btn-arrow--left" />
-        </button>
-        <div className="carousel__container">
-          <ul className="carousel__slide-list">
-            {items.map((pos, i) => (
-              <CarouselSlideItem
-                key={i}
-                idx={i}
-                pos={pos}
-                activeIdx={activeIdx}
-              />
-            ))}
-          </ul>
-        </div>
-        <button
-          className="carousel__btn carousel__btn--next"
-          onClick={() => nextClick()}
-        >
-          <i className="carousel__btn-arrow carousel__btn-arrow--right" />
-        </button>
-        <div className="carousel__dots">
-          {items.slice(0, length).map((pos, i) => (
-            <button
-              key={i}
-              onClick={() => handleDotClick(i)}
-              className={i === activeIdx ? "dot active" : "dot"}
-            />
-          ))}
-        </div>
-      </div>
+    <div className="carousel">
+      <button onClick={handlePrevClick} disabled={startIndex === 0}>
+        Prev
+      </button>
+      {data.length > 0 &&
+        data.slice(startIndex, endIndex).map((product) => (
+          <div key={product.name}>
+            <img src={product.picture} alt={product.name} />
+            <p>{product.name}</p>
+          </div>
+        ))}
+      <button
+        onClick={handleNextClick}
+        disabled={startIndex === data.length - 3}
+      >
+        Next
+      </button>
     </div>
   );
 }
+
+export default ProductCarousel;
